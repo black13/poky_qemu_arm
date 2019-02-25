fa987efc49264a2fdcc9fed80925efd634b875a2
jjosburn <jjosburn@gmail.com>
2019-02-25 08:26:54 -0500

    local conf added

--- build.sh
+++ build.sh
@@ -9,7 +9,8 @@ git checkout tags/yocto-2.6.1 -b my-yocto-2.6.1
 
 cd ..
 mkdir build/conf
-
+#cp ./local.conf.mine ./build/conf/local.conf
 source ./poky/oe-init-build-env build
+#bitbake -c populate_sdk
 
 
