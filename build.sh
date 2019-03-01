85a1bf99282b1e7f961226c40c2fe196d92c2a53
Black13 <jjosburn@gmail.com>
2019-02-28 20:57:51 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ git tag
 git checkout tags/yocto-2.6.1 -b my-yocto-2.6.1
 
 cd ..
-mkdir build/conf
+mkdir -p build/conf
 #cp ./local.conf.mine ./build/conf/local.conf
 source ./poky/oe-init-build-env build
 #bitbake -c populate_sdk
